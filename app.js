f7f3f8d41a1bafeef70776ae98ce257ced6f41e4
Putu Mahendra <putuhendramahendra@gmail.com>
2020-09-12 08:53:49 +0800

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-( (age , name) => {
+((age, name) => {
 
-const userId = 1;
+    const userId = 1;
 
->>>>>>> 735328806186d3c2f3ae97c8b8bc6847a4e1f77e
-console.log(`My Age ${age}, and i'm ${name}`);
+    console.log(`My Age ${age}, and i'm ${name}`);
 })(20, 'putu');
